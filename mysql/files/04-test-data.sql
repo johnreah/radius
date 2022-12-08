91c7606e85667c21b61d8b98cd3bb7f31da9d05b
John Reah <john.reah@johnreah.com>
2022-12-08 13:01:41 +0000

    Test data

--- mysql/files/04-test-data.sql
+++ mysql/files/04-test-data.sql
@@ -1,1 +1,3 @@
 INSERT INTO nas VALUES (NULL , '10.10.10.1', 'nas client name', 'other', NULL , 'passwordnas', NULL , NULL , 'RADIUS Client nas1');
+INSERT INTO radcheck VALUES (NULL , 'user1', 'Cleartext-Password', ':=', 'passworduser1');
+INSERT INTO radreply VALUES (NULL , 'user1', 'Framed-IP-Address', ':=', '10.10.10.2');
