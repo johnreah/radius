c0a06f0860c7d71bc87ef9e77771bdf67a6bcdb6
johnreah <john.reah@johnreah.com>
2022-12-09 17:51:12 +0000

    .gitignore

--- client/radius/src/main/java/radius/App.java
+++ client/radius/src/main/java/radius/App.java
@@ -1,7 +1,6 @@
 package radius;
 
 import org.tinyradius.util.RadiusClient;
-import org.tinyradius.util.RadiusEndpoint;
 import org.tinyradius.util.RadiusException;
 
 import java.io.IOException;
@@ -18,17 +17,17 @@ public class App
     private static String SHARED_SECRET = "testing123";
     private static String USERNAME = "user1";
     private static String PASSWORD = "passworduser1";
-    private static int NUM_THREADS = 2;
-    private static int ITERATIONS_PER_THREAD = 1;
-    private static int MAX_SLEEP_MILLIS = 1000;
+    private static int NUM_THREADS = 100;
+    private static int ITERATIONS_PER_THREAD = 10;
+    private static int MAX_SLEEP_MILLIS = 0;
 
     public static void main( String[] args ) throws IOException, RadiusException, InterruptedException {
 
         AtomicInteger numTasksComplete = new AtomicInteger();
         ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool(NUM_THREADS);
 
-        System.out.println(String.format("Threads=%d, iterations per thread=%d, mean delay per iteration=%dms. Expected duration=%.1fs, total tests=%d",
-                NUM_THREADS, ITERATIONS_PER_THREAD, MAX_SLEEP_MILLIS/2, (double)ITERATIONS_PER_THREAD * MAX_SLEEP_MILLIS / 2 / 1000, NUM_THREADS * ITERATIONS_PER_THREAD));
+        System.out.println(String.format("Threads=%d, iterations per thread=%d, mean delay per iteration=%dms, total tests=%d",
+                NUM_THREADS, ITERATIONS_PER_THREAD, MAX_SLEEP_MILLIS/2, NUM_THREADS * ITERATIONS_PER_THREAD));
 
         long startTime = System.currentTimeMillis();
         for (int t = 0; t < NUM_THREADS; t++) {
@@ -37,7 +36,8 @@ public class App
                 Random random = new Random();
                 for (int i = 0; i < ITERATIONS_PER_THREAD; i++) {
                     try {
-                        Thread.sleep(random.nextInt(MAX_SLEEP_MILLIS));
+                        if (MAX_SLEEP_MILLIS > 0)
+                            Thread.sleep(random.nextInt(MAX_SLEEP_MILLIS));
                         boolean authenticated = radiusClient.authenticate(USERNAME, PASSWORD);
                         System.out.println(String.format("thread=%d, iteration=%d, authenticated=%s, completed=%d", Thread.currentThread().getId(), i, authenticated, numTasksComplete.incrementAndGet()));
                     } catch (InterruptedException | IOException | RadiusException e) {
@@ -47,8 +47,10 @@ public class App
             });
         }
         threadPoolExecutor.shutdown();
-        threadPoolExecutor.awaitTermination(20, TimeUnit.SECONDS);
-        System.out.println(String.format("Elapsed time=%.1fs, tests completed=%d", ((double)System.currentTimeMillis() - startTime) / 1000, numTasksComplete.get()));
+        threadPoolExecutor.awaitTermination(300, TimeUnit.SECONDS);
+
+        double elapsedTimeSeconds = ((double)System.currentTimeMillis() - startTime) / 1000;
+        System.out.println(String.format("Elapsed time=%.1fs, tests completed=%d, rate=%.1f auths/sec", elapsedTimeSeconds, numTasksComplete.get(), (double)numTasksComplete.get() / elapsedTimeSeconds));
 
     }
 }
